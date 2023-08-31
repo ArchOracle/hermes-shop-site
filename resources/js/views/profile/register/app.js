b4b842352ad6053f83a99669628ea22ec5797cd0
ArchOracle <wowakk98@gmail.com>
2023-08-31 21:07:32 +0300

    Заложил страницу регистрации.

---
+++ resources/js/views/profile/register/app.js
@@ -0,0 +1,7 @@
+import '/resources/js/bootstrap';
+
+import {createApp} from 'vue'
+
+import App from '../../../vue/views/profile/register/App.vue'
+
+createApp(App).mount("#app")
---
+++ resources/js/vue/views/profile/register/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Header></Header>
+  <div class="content">
+    Страница регистрации
+  </div>
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from "/resources/js/vue/components/Header.vue";
+import Footer from "/resources/js/vue/components/Footer.vue";
+export default {
+  name: "App.vue",
+  components: {Header, Footer}
+}
+</script>
+
+<style scoped>
+
+.content {
+  min-height: 600px;
+  border: solid 1px black;
+  margin: 10px;
+}
+</style>
