9d4f6f0bb2d89cb50b999ec0843548e4c3705bf4
ArchOracle <wowakk98@gmail.com>
2023-08-31 20:48:01 +0300

    Разместил хедер, контентную часть и футер на имеющихся страницах.

--- resources/js/vue/views/admin/App.vue
+++ resources/js/vue/views/admin/App.vue
@@ -1,13 +1,25 @@
 <template>
-  Заглушка для админки
+  <Header></Header>
+  <div class="content">
+    Заглушка для админки
+  </div>
+  <Footer></Footer>
 </template>
 
 <script>
+import Header from "../../components/Header.vue";
+import Footer from "../../components/Footer.vue";
 export default {
-  name: "App.vue"
+  name: "App.vue",
+  components: {Header, Footer}
 }
 </script>
 
 <style scoped>
 
+.content {
+  min-height: 600px;
+  border: solid 1px black;
+  margin: 10px;
+}
 </style>
