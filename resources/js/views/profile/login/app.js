c18bb6e10aff7b75d19c85c427896d5bfcc1d873
ArchOracle <wowakk98@gmail.com>
2023-08-31 21:07:20 +0300

    Заложил страницу входа в систему.

---
+++ resources/js/views/profile/login/app.js
@@ -0,0 +1,7 @@
+import '/resources/js/bootstrap';
+
+import {createApp} from 'vue'
+
+import App from '../../../vue/views/profile/login/App.vue'
+
+createApp(App).mount("#app")
---
+++ resources/views/profile/login.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Hermes</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+<div id="app"></div>
+@vite('resources/js/views/profile/login/app.js')
+</body>
+</html>
